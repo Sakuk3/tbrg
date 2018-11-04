109fc3c1e6b74fdddfb2468f2e65b8d3c11fbb60
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-11-04 16:52:40 +0100

    draw sprite

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-display_width = 800
-display_height = 600
+display_width = 1600
+display_height = 900
 
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0) 
+red = (255,0,0)
--- game.py
+++ game.py
@@ -5,11 +5,12 @@ def main():
     pygame.init()
     gameDisplay = pygame.display.set_mode((config.display_width,config.display_height))
     pygame.display.set_caption('TBRG')
+    gameDisplay.fill(config.white)
     clock = pygame.time.Clock()
+    all_sprites_list = pygame.sprite.Group(Car(config.display_width/2,config.display_height/2))
     while True:
         x = pygame.event.get()
         for event in x:
-            print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -17,7 +18,26 @@ def main():
                 if event.key == pygame.K_q:
                     pygame.quit()
                     quit()
+
+        gameDisplay.fill(config.white)
+        all_sprites_list.update()
+        all_sprites_list.draw(gameDisplay)
+        pygame.display.update()
+
     pygame.quit()
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.image.load("assets/car.png").convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x - self.rect.width/2
+        self.rect.y = pos_y - self.rect.height/2
+
+
+
+
+
 
 if __name__ == "__main__":
     main()

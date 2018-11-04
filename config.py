a8c31ff31459495ff54e8c4ee535a85a5168e8c2
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-11-04 20:34:19 +0100

    added basic movement

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 display_width = 1600
 display_height = 900
 
+fps = 60
+title = "tbrg"
+
+max_acceleration = 3
+max_speed = 10
+friction = 1
+
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import pygame
+import numpy
 import time
 import config
 def main():
     pygame.init()
     gameDisplay = pygame.display.set_mode((config.display_width,config.display_height))
-    pygame.display.set_caption('TBRG')
+    pygame.display.set_caption(config.title)
     gameDisplay.fill(config.white)
     clock = pygame.time.Clock()
     all_sprites_list = pygame.sprite.Group(Car(config.display_width/2,config.display_height/2))
@@ -20,7 +21,8 @@ def main():
                     quit()
 
         gameDisplay.fill(config.white)
-        all_sprites_list.update()
+        all_sprites_list.update(0,0)
+
         all_sprites_list.draw(gameDisplay)
         pygame.display.update()
 
@@ -30,11 +32,28 @@ class Car(pygame.sprite.Sprite):
     def __init__(self,pos_x,pos_y):
         super().__init__()
         self.image = pygame.image.load("assets/car.png").convert()
+        self.image = pygame.transform.rotate(self.image, 10)
         self.rect = self.image.get_rect()
+        self.momentum = Momentum([0,0])
         self.rect.x = pos_x - self.rect.width/2
         self.rect.y = pos_y - self.rect.height/2
 
+    def update(self,acceleration_percentage,break_percentage):
+        #self.speed.accelerate(config.max_acceleration * acceleration_percentage)
+        self.rect.y += 10
+        self.rect.x += 10
 
+class Momentum():
+    def __init__(self,start_momentum):
+        self.speed = Vector2D(start_momentum)
+    def accelerate(acceleration):
+        self.speed.add(acceleration)
+
+class Vector2D():
+    def __init__(self,start_vector):
+        self.vector = start_vector
+    def add(vector):
+        self.vector += vector
 
 
 

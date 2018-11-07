61511b2bef615d49a20414d61aa31cf5c18221f3
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-11-07 14:17:28 +0100

    Carmovement "finished"

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-display_width = 400
-display_height = 400
+display_width = 1600
+display_height = 900
 
 fps = 60
 title = "tbrg"
 
 #Sprites
 car_sprite_location = "assets/car_32x.png"
-max_acceleration = 5
+max_acceleration = 3
 max_velocity = 10
-max_steering = 30
-free_deceleration = 2
-brake_deceleration = 10
-friction = 1
+max_steering = 3
+free_deceleration = 0.1
+max_brake_deceleration = 10
+car_length = 10
 
 black = (0,0,0)
 white = (255,255,255)
--- game.py
+++ game.py
@@ -11,12 +11,12 @@ class Game:
         self.gameDisplay.fill(config.white)
         self.clock = pygame.time.Clock()
         self.cars = pygame.sprite.Group()
-        self.cars.add(Car(config.display_width/2,config.display_height/2))
+        self.cars.add(Car(config.display_height/2,config.display_width/2))
     def run(self):
+        breaking = 0
+        acceleration = 0
+        steering = 0
         while True:
-            breaking = 0
-            acceleration = 0
-            steering = 0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -26,14 +26,25 @@ class Game:
                         pygame.quit()
                         quit()
                     elif event.key == pygame.K_w:
-                        acceleration = 1
-                        print('1')
-                    elif event.key == pygame.K_a:
-                        steering = 1
-                    elif event.key == pygame.K_d:
-                        steering = --1
+                        acceleration += 1
                     elif event.key == pygame.K_s:
-                        breaking = 1
+                        acceleration -= 1
+                    elif event.key == pygame.K_a:
+                        steering -= 1
+                    elif event.key == pygame.K_d:
+                        steering += 1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        quit()
+                    elif event.key == pygame.K_w:
+                        acceleration -= 1
+                    elif event.key == pygame.K_s:
+                        acceleration += 1
+                    elif event.key == pygame.K_a:
+                        steering += 1
+                    elif event.key == pygame.K_d:
+                        steering -= 1
 
             self.cars.update(acceleration,steering,breaking)
             self.gameDisplay.fill(config.white)
@@ -44,32 +55,50 @@ class Game:
         pygame.quit()
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self, x, y, angle=0.0, length=40):
+    def __init__(self, x, y, angle=0.0):
         super().__init__()
         self.image = pygame.image.load(config.car_sprite_location)
+        self.orig_image = self.image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.velocity = Vector2(0.0, 0.0)
         self.position = Vector2(x,y)
         self.angle = angle
-        self.length = length
 
     def update(self,acceleration,steering,breaking):
+        #accelerate Car
         self.velocity += (acceleration * config.max_acceleration, 0)
+        #if car is faster than max velocity set velocity to max velocity
         self.velocity.x = max(-config.max_velocity, min(self.velocity.x, config.max_velocity))
+        #slow car
+        isVelocityPositive = self.velocity.x > 0
+        negativeAcceleration = breaking * config.max_brake_deceleration
+        if(acceleration == 0):
+            negativeAcceleration += config.free_deceleration
 
-        if steering:
-            turning_radius = self.length / tan(radians(steering*config.max_steering))
+        if(abs(self.velocity.x) - negativeAcceleration) < 0.0:
+            self.velocity.x = 0
+        elif isVelocityPositive:
+            self.velocity.x -= negativeAcceleration
+        elif not isVelocityPositive:
+            self.velocity.x += negativeAcceleration
+
+
+
+
+        if steering != 0:
+            turning_radius = config.car_length / tan(radians(steering*config.max_steering))
             angular_velocity = self.velocity.x / turning_radius
         else:
             angular_velocity = 0
-
+        self.angle += degrees(angular_velocity)
         self.position += self.velocity.rotate(-self.angle)
         self.rect.y = self.position[0] - self.rect.height/2
         self.rect.x = self.position[1] - self.rect.width/2
-        self.angle += degrees(angular_velocity)
-        self.image = pygame.transform.rotate(self.image, self.angle)
+        self.image = pygame.transform.rotozoom(self.orig_image, -self.angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        #self.image = pygame.transform.rotate(self.image, self.angle)
 
 
 class Tile(pygame.sprite.Sprite):
